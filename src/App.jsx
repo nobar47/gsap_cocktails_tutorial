850ef26c3a411b40d8cb4cfb9687253e5e22c735
Nobar Hassan <nobar.hassan@gmail.com>
2025-11-04 16:54:54 +0300

    implement menu and contact section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
 import About from './components/About'
 import Art from './components/Art'
+import Menu from './components/Menu'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
+      <Contact />
     </main>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,83 @@
+import gsap from 'gsap'
+import { useGSAP } from "@gsap/react"
+import { SplitText } from 'gsap/all'
+import { openingHours, socials } from "../constants"
+
+const Contact = () => {
+
+    useGSAP(() => {
+        const titleSplit = SplitText.create('#contact h2', { type: 'words' });
+
+        const timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#contact',
+                start: 'top center',
+            },
+            ease: 'power1.inOut'
+        })
+
+        timeline
+            .from(titleSplit.words, {
+                opacity: 0, yPercent: 100, stagger: 0.02
+            })
+            .from('#contact h3, #contact p', {
+                opacity: 0, yPercent: 100, stagger: 0.02
+            })
+            .to('#f-right-leaf', {
+                y: '-50', duration: 1, ease: 'power1.inOut'
+            })
+            .to('#f-left-leaf', {
+                y: '-50', duration: 1, ease: 'power1.inOut'
+            }, '<')
+
+    })
+    return (
+        <footer id="contact">
+            <img src="/images/footer-right-leaf.png" alt="leaf-right" id="f-right-leaf" />
+            <img src="/images/footer-left-leaf.png" alt="leaf-left" id="f-left-leaf" />
+
+            <div className="content">
+                <h2>Where to find us</h2>
+
+                <div>
+                    <h3>Visit Our Bar</h3>
+                    <p>12, Grand Street. #404, Kurdistan, Duhok 42001</p>
+                </div>
+
+                <div>
+                    <h3>Contact Us</h3>
+                    <p>(750) 473-2233</p>
+                    <p>hello@jsmcocktail.com</p>
+                </div>
+
+                <div>
+                    <h3>Open Every Day</h3>
+                    {openingHours.map((time) => (
+                        <p key={time.day}>
+                            {time.day} : {time.time}
+                        </p>
+                    ))}
+                </div>
+
+                <div>
+                    <h3>Socials</h3>
+
+                    <div className="flex-center gap-5">
+                        {socials.map((social) => (
+                            <a href={social.url}
+                                key={social.name}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                aria-label={social.name}
+                            >
+                                <img src={social.icon} alt={social.name} />
+                            </a>
+
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Contact
